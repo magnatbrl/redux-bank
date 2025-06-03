15698c425badcc08e8f565b07c7085130e5b1616
magnatbrl <magnat@gmx.net>
2025-06-03 18:49:27 +0200

    only redux

--- src/configurStore/store.js
+++ src/configurStore/store.js
@@ -1,5 +1,5 @@
+import { legacy_createStore as createStore } from "redux"
 import { accountReducer } from "../reducer/accountReducer"
-import { createStore } from "../redux/createStore"
 
 const initialState = {
     balance: 0
